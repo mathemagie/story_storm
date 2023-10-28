faa8dd17462653ee6e94274e8d932bbca9b8495a
aurelien.fache <aurelien.fache@saflab.io>
2023-10-28 17:12:30 +0200

    Black linter

--- index.py
+++ index.py
@@ -17,58 +17,70 @@ llm = OpenAI(temperature=0.9)
 
 
 def generate_story(text):
-  """Generate a story using the langchain library and OpenAI's GPT-3 model."""
-  prompt = PromptTemplate(input_variables=["text"],
-                          template=""" 
+    """Generate a story using the langchain library and OpenAI's GPT-3 model."""
+    prompt = PromptTemplate(
+        input_variables=["text"],
+        template=""" 
          You are a fun and seasoned storyteller. Generate a story for me about {text}.
-                 """)
-  story = LLMChain(llm=llm, prompt=prompt)
-  return story.run(text=text)
+                 """,
+    )
+    story = LLMChain(llm=llm, prompt=prompt)
+    return story.run(text=text)
 
 
 def generate_audio(text, voice):
-  """Convert the generated story to audio using the Eleven Labs API."""
-  audio = generate(text=text, voice=voice,  model='eleven_multilingual_v1', api_key=eleven_api_key)
-  return audio
+    """Convert the generated story to audio using the Eleven Labs API."""
+    audio = generate(
+        text=text, voice=voice, model="eleven_multilingual_v1", api_key=eleven_api_key
+    )
+    return audio
 
 
 def generate_images(story_text):
-  """Generate images using the story text using the Replicate API."""
-  output = replicate.run(
-    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-    input={"prompt": story_text})
-  return output
+    """Generate images using the story text using the Replicate API."""
+    output = replicate.run(
+        "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+        input={"prompt": story_text},
+    )
+    return output
 
 
 def app():
-  st.title("Story Storm")
+    st.title("Story Storm")
 
-  with st.form(key='my_form'):
-    text = st.text_input(
-      "Enter a word to generate a story",
-      max_chars=None,
-      type="default",
-      placeholder="Enter a word to generate a story",
-    )
-    options = [
-      "Bella", "Antoni", "Arnold", "Adam", "Domi", "Elli", "Josh", "Rachel",
-      "Sam"
-    ]
-    voice = st.selectbox("Select a voice", options)
+    with st.form(key="my_form"):
+        text = st.text_input(
+            "Enter a word to generate a story",
+            max_chars=None,
+            type="default",
+            placeholder="Enter a word to generate a story",
+        )
+        options = [
+            "Bella",
+            "Antoni",
+            "Arnold",
+            "Adam",
+            "Domi",
+            "Elli",
+            "Josh",
+            "Rachel",
+            "Sam",
+        ]
+        voice = st.selectbox("Select a voice", options)
 
-    if st.form_submit_button("Submit"):
-      with st.spinner('Generating story...'):
-        story_text = generate_story(text)
-        audio = generate_audio(story_text, voice)
+        if st.form_submit_button("Submit"):
+            with st.spinner("Generating story..."):
+                story_text = generate_story(text)
+                audio = generate_audio(story_text, voice)
 
-      st.audio(audio, format='audio/mp3')
-      images = generate_images(story_text)
-      for item in images:
-        st.image(item)
+            st.audio(audio, format="audio/mp3")
+            images = generate_images(story_text)
+            for item in images:
+                st.image(item)
 
-  if not text or not voice:
-    st.info("Please enter a word and select a voice")
+    if not text or not voice:
+        st.info("Please enter a word and select a voice")
 
 
-if __name__ == '__main__':
-  app()
+if __name__ == "__main__":
+    app()
